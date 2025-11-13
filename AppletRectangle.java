4e753ef970949cb9a568e2e16db86eae4b3f8027
Af1ah <muhammadaflah23524@gmail.com>
2025-11-13 15:22:38 +0530

    applet updated

--- AppletRectangle.java
+++ AppletRectangle.java
@@ -2,13 +2,28 @@ import java.awt.*;
 import java.applet.*;
 /*
 <applet code="AppletRectangle.class" width="500" height="500">
-</applet>
+        <param name="x" value="50">
+        <param name="y" value="50">
+        <param name="w" value="150">
+        <param name="h" value="250">
+        <param name="r" value="200">
+        <param name="g" value="000">
+        <param name="b" value="010">
+        
+    </applet>
 */
 public class AppletRectangle extends Applet{
-    public void paint(Graphics g){
-        Color c = new Color(200,010,00);
-        g.setColor(c);
-        g.fillRect(250, 250, 300, 200);
+    public void paint(Graphics G){
+        int x = Integer.parseInt(getParameter("x"));
+        int y = Integer.parseInt(getParameter("y"));
+        int r = Integer.parseInt(getParameter("r"));
+        int g = Integer.parseInt(getParameter("g"));
+        int b = Integer.parseInt(getParameter("b"));
+        int w = Integer.parseInt(getParameter("w"));
+        int h = Integer.parseInt(getParameter("h"));
+        Color c = new Color(r,g,b);
+        G.setColor(c);
+        G.fillRect(x, y, h, w);
     }
     
 }
