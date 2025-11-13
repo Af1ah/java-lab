a73cd83815bec6bc91089343f7e3f9bcb54082fe
Af1ah <muhammadaflah23524@gmail.com>
2025-11-13 11:38:50 +0530

    added thread.java

--- Thread.java
+++ Thread.java
@@ -1,3 +1,40 @@
-public class Thread {
-    
+class OddThread extends Thread {
+
+    private int limit;
+    OddThread(int lim){
+        super();
+        limit=lim;
+       
+    }
+
+    public void run(){
+    try{
+        for(int i = 0;i<=limit;i++){
+        if (i%2!=0) {
+            System.out.println("Odd: "+i);
+            Thread.sleep(1000);
+        }
+    }
+    }catch(Exception e){}
+    }
 }
+class MyThread {
+    
+    public static void main(String[] args) {
+       
+        OddThread ot = new OddThread(5);
+
+        
+        ot.start();
+        try{
+        for(int i = 0;i<=5;i++){
+        if (i%2==0) {
+            System.out.println("Even: "+i);
+           Thread.sleep(1000);
+        }
+
+
+    }
+    }catch(Exception e){}
+    
+}}
